63ad47571d0ee397da1c7717df28c4f8a4d462ca
Owen Den Uyl <odenuyl@gmail.com>
2026-01-10 17:29:45 -0500

    tweaked color.h and added comments

--- include/color.h
+++ include/color.h
@@ -5,14 +5,15 @@ struct rgb_t {
     float r, g, b, a;
 };
 
+// creates color shortcuts without rgb function step, slightly more efficient
 namespace Engine::Color {
     inline constexpr rgb_t RED = {1, 0, 0, 1};
-    inline constexpr rgb_t ORANGE = {1, 118/255, 0, 1};
+    inline constexpr rgb_t ORANGE = {1, .463, 0, 1};
     inline constexpr rgb_t YELLOW = {1, 1, 0, 1};
     inline constexpr rgb_t GREEN = {0, 1, 0, 1};
     inline constexpr rgb_t BLUE = {0, 0, 1, 1};
-    inline constexpr rgb_t INDIGO = {63/255, 0, 1, 1};
-    inline constexpr rgb_t VIOLET = {176/255, 0, 1, 1};
+    inline constexpr rgb_t INDIGO = {.247, 0, 1, 1};
+    inline constexpr rgb_t VIOLET = {.69, 0, 1, 1};
     inline constexpr rgb_t WHITE = {1, 1, 1, 1};
     inline constexpr rgb_t BLACK = {0, 0, 0, 1};
     inline constexpr rgb_t GREY = {.5, .5, .5, 1};
